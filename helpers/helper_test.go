f6de8f2fd215d0021c2f72cd3c98b2dab649a50c
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2026-06-08 08:13:53 +0530

    test: update helper test mock to match new interface signature

--- helpers/helper_test.go
+++ helpers/helper_test.go
@@ -1,17 +1,20 @@
 package helpers
 
 import (
+	"context"
 	"testing"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/Srajan-Sanjay-Saxena/RabbitMqWrapper-Service-Go/channel"
 )
 
-// Verify IRabbitConnection interface compiles correctly
 func TestIRabbitConnectionInterface(t *testing.T) {
 	var _ IRabbitConnection = (*mockConn)(nil)
 }
 
 type mockConn struct{}
 
-func (m *mockConn) GetChannel() (*amqp.Channel, error) { return nil, nil }
-func (m *mockConn) Shutdown() error                    { return nil }
+func (m *mockConn) GetChannel(ctx context.Context, onClose channel.OnChannelClose) (*amqp.Channel, error) {
+	return nil, nil
+}
+func (m *mockConn) Shutdown() error { return nil }
