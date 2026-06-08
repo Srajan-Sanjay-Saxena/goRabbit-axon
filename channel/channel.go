7aa1c4594e25eba549316106da18caa9ca98274b
Srajan-Sanjay-Saxena <invinciblecoder071723@gmail.com>
2026-06-08 08:12:06 +0530

    refactor: channel handler takes onClose per call, not per instance

--- channel/channel.go
+++ channel/channel.go
@@ -7,24 +7,22 @@ import (
 	amqp "github.com/rabbitmq/amqp091-go"
 )
 
-type OnChannelClose func(deadCh *amqp.Channel, conn *amqp.Connection)
+type OnChannelClose func(conn *amqp.Connection)
 
 type ChannelHandler struct {
 	logger  *logger.Logger
-	onClose OnChannelClose
 }
 
-func NewChannelHandler(log *logger.Logger, onClose OnChannelClose) *ChannelHandler {
+func NewChannelHandler(log *logger.Logger) *ChannelHandler {
 	if log == nil {
 		log = logger.New(logger.Production)
 	}
 	return &ChannelHandler{
 		logger:  log,
-		onClose: onClose,
 	}
 }
 
-func (ch *ChannelHandler) GetChannel(ctx context.Context, conn *amqp.Connection) (*amqp.Channel, error) {
+func (ch *ChannelHandler) GetChannel(ctx context.Context, conn *amqp.Connection , onClose OnChannelClose) (*amqp.Channel, error) {
 	if conn == nil || conn.IsClosed() {
 		return nil, amqp.ErrClosed
 	}
@@ -34,20 +32,20 @@ func (ch *ChannelHandler) GetChannel(ctx context.Context, conn *amqp.Connection)
 		return nil, err
 	}
 
-	go ch.HandleChannelClose(ctx, channel, conn)
+	go ch.HandleChannelClose(ctx, channel, conn, onClose)
 
 	return channel, nil
 }
 
-func (ch *ChannelHandler) HandleChannelClose(ctx context.Context, channel *amqp.Channel, conn *amqp.Connection) {
+func (ch *ChannelHandler) HandleChannelClose(ctx context.Context, channel *amqp.Channel, conn *amqp.Connection , onClose OnChannelClose) {
 	closeCh := channel.NotifyClose(make(chan *amqp.Error, 1))
 	select {
 	case err := <-closeCh:
 		if err != nil {
 			ch.logger.Warn("channel closed unexpectedly", "error", err)
 		}
-		if ch.onClose != nil {
-			ch.onClose(channel, conn)
+		if onClose != nil {
+			onClose(conn)
 		}
 	case <-ctx.Done():
 		return
